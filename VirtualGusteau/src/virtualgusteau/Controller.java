a946398e7f1220ca90c4390f966b0ff0348ea81a
robert.krantz <robert.krantz@0de4426a-6c4a-0410-a967-598ff313a736>
2008-04-09 09:17:09 +0000

    Skapat ett skal att utgå ifrån och fått igång MVC // Robert

--- VirtualGusteau/src/virtualgusteau/Controller.java
+++ VirtualGusteau/src/virtualgusteau/Controller.java
@@ -1,10 +1,27 @@
 
 package virtualgusteau;
 
+import java.awt.event.*;
+import javax.swing.*;
 /**
  *
  * @author rkrantz
  */
-public class Controller {
-
+public class Controller implements ActionListener {
+    private Model model;
+    public Controller(Model model) {
+        this.model = model;
+    }
+    public void actionPerformed(ActionEvent event) {
+        int cmd = Integer.parseInt(event.getActionCommand());
+        
+        switch(cmd) {
+            case 1:
+                System.exit(0);
+                break;
+            case 2:
+                JTextField t = (JTextField)event.getSource();
+                model.parse(t.getText());
+        }
+    }
 }
--- VirtualGusteau/src/virtualgusteau/View.java
+++ VirtualGusteau/src/virtualgusteau/View.java
@@ -1,10 +1,73 @@
 
 package virtualgusteau;
 
-/**
- *
- * @author rkrantz
- */
-public class View {
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
 
+/**
+ * @author Robert Krantz
+ */
+public class View extends JFrame implements Observer {
+    
+    private Controller controller;
+    private Model model;
+    
+    private JTextField textField;
+    private JTextArea textArea;
+        
+    public View(Model model) {
+        this.model = model;
+        controller = new Controller(model);
+        
+        setTitle("Virtual Gusteau");
+        
+        
+        
+        /*
+         * Create the menu
+         */
+            JMenuBar menubar = new JMenuBar();
+            JMenu file = new JMenu("File");
+
+            JMenuItem fileClose = new JMenuItem("Close");
+            fileClose.setActionCommand("1");
+            fileClose.addActionListener(controller);
+            
+            file.add(fileClose);
+            menubar.add(file);
+            
+            setJMenuBar(menubar);
+        
+                
+        textField = new JTextField();
+        textField.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
+        textField.setActionCommand("2");
+        textField.addActionListener(controller);
+                
+        JScrollPane pane = new JScrollPane();
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
+        
+        pane.getViewport().add(textArea);
+        
+        JSeparator separator = new JSeparator();
+        separator.setForeground(Color.gray);
+        
+        add(pane);
+        add(textField,BorderLayout.SOUTH);
+
+        setSize(400,400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+    public void update(Observable o, Object arg) {
+        textArea.append(model.getArea() + "\n");
+        textField.setText(model.getField());
+    }
 }
