356fc5795bf0c6f760c8933d72133910cc66d494
robert.krantz <robert.krantz@0de4426a-6c4a-0410-a967-598ff313a736>
2008-04-11 15:22:19 +0000

    // Robert

---
+++ VirtualGusteau/src/virtualgusteau/AboutDialog.java
@@ -0,0 +1,91 @@
+package virtualgusteau;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author Robert
+ */
+public class AboutDialog extends JDialog {
+    public AboutDialog() {
+        setTitle("About Notes");
+        
+        setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+        
+        add(Box.createRigidArea(new Dimension(0,10)));
+        
+        JLabel label = new JLabel("Virtual Gusteau");
+        label.setFont(new Font("Tahoma",Font.BOLD,20));
+        label.setAlignmentX(0.5f);
+        add(label);
+        
+        add(Box.createRigidArea(new Dimension(0,10)));
+        
+        printNames();
+        
+        add(Box.createRigidArea(new Dimension(0,20)));
+        
+        JButton close = new JButton("Close");
+        close.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        close.setAlignmentX(0.5f);
+        add(close);
+        
+        setModalityType(ModalityType.APPLICATION_MODAL);
+        
+        setResizable(false);
+        
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(300,250);
+    }
+    private void printNames() {
+        JLabel name = new JLabel("Anders Lorentzen");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+        
+        name = new JLabel("Magnus Spånggård");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+        
+        name = new JLabel("Olof Millberg");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+        
+        name = new JLabel("Patrik Björkman");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+        
+        name = new JLabel("Robert Krantz");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+        
+        name = new JLabel("Tobias Alette");
+        name.setFont(new Font("Tahoma",Font.PLAIN,13));
+        name.setAlignmentX(0.5f);
+        add(name);
+    }
+}
--- VirtualGusteau/src/virtualgusteau/View.java
+++ VirtualGusteau/src/virtualgusteau/View.java
@@ -2,6 +2,7 @@
 package virtualgusteau;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
 import javax.swing.border.*;
@@ -10,6 +11,12 @@ import javax.swing.ImageIcon;
 import java.awt.image.*;
 import javax.imageio.ImageIO;
 import java.io.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import java.text.DateFormat;
 
 
 /**
@@ -24,29 +31,42 @@ public class View extends JFrame implements Observer {
     private JPanel left;
     
     private JTextField textField;
-    private JTextArea textArea;
+    private JScrollPane chatPane;
+    private JTextArea chatArea;
     private JPanel pictureFrame;
+    private JTextArea recipeArea;
+    private JScrollPane recipePane;
+    
+    private Calendar cal;
+    private Date date;
+    private Locale locale;
     
     public View(Model model) {
+        
+        try {
+	    // Set cross-platform Java L&F (also called "Metal")
+        UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+           // handle exception
+        }
+        catch (ClassNotFoundException e) {
+           // handle exception
+        }
+        catch (InstantiationException e) {
+           // handle exception
+        }
+        catch (IllegalAccessException e) {
+           // handle exception
+        }
+        
         this.model = model;
         controller = new Controller(model);
         
-        setTitle("Virtual Gusteau");       
+        setTitle("Virtual Gusteau");      
         
-        /*
-         * Create the menu
-         */
-            JMenuBar menubar = new JMenuBar();
-            JMenu file = new JMenu("File");
-
-            JMenuItem fileClose = new JMenuItem("Close");
-            fileClose.setActionCommand("1");
-            fileClose.addActionListener(controller);
-            
-            file.add(fileClose);
-            menubar.add(file);
-            
-            setJMenuBar(menubar);
+        createMenu();
         
         left = new JPanel();
         right = new JPanel();
@@ -58,39 +78,58 @@ public class View extends JFrame implements Observer {
         textField.setActionCommand("2");
         textField.addActionListener(controller);
                 
-        JScrollPane pane = new JScrollPane();
-        textArea = new JTextArea();
-        textArea.setEditable(false);
+        chatPane = new JScrollPane();
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                textField.requestFocusInWindow();
+            }
+
+            public void focusLost(FocusEvent e) {
+            } 
+        });
         
-        textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        textArea.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
+        chatArea.setLineWrap(true);
+        chatArea.setWrapStyleWord(true);
+        chatArea.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
         
-        pane.getViewport().add(textArea);
+        chatPane.getViewport().add(chatArea);
         
         JSeparator separator = new JSeparator();
         separator.setForeground(Color.gray);
         
-        left.add(pane);
+        left.add(chatPane);
         left.add(textField,BorderLayout.SOUTH);
         
         pictureFrame = new JPanel();
         pictureFrame.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
-        pictureFrame.setBounds(63,13,124,124);
+        pictureFrame.setBounds(23,23,204,170);
                                 
         right.setPreferredSize(new Dimension(250,400));
         right.setBorder(BorderFactory.createEtchedBorder());
         right.setLayout(null);
         
-        ImageIcon icon = new ImageIcon("Gusteau_icon.jpg");
+        ImageIcon icon = new ImageIcon("Gusteau_icon_200px.jpg");
         JLabel label = new JLabel();
         label.setIcon(icon);
         
         pictureFrame.setLayout(null);
-        label.setBounds(2,2,120,120);
+        label.setBounds(2,2,200,166);
         pictureFrame.add(label);
         
+        recipePane = new JScrollPane();
+        recipeArea = new JTextArea();
+        recipeArea.setWrapStyleWord(true);
+        recipeArea.setLineWrap(true);
+        recipeArea.setEditable(true);
+        
+        recipePane.getViewport().add(recipeArea);
+        recipePane.setBounds(23,206,204,320);
+        recipePane.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        
         right.add(pictureFrame);
+        right.add(recipePane);
         
         add(left);
         add(right,BorderLayout.EAST);
@@ -100,8 +139,63 @@ public class View extends JFrame implements Observer {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
     }
+    public void createMenu() {
+        JMenuBar menubar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F);
+        JMenu help = new JMenu("Help");
+        help.setMnemonic(KeyEvent.VK_H);
+        JMenu tools = new JMenu("Tools");
+        tools.setMnemonic(KeyEvent.VK_T);
+
+        JMenuItem fileClose = new JMenuItem("Close");
+        fileClose.setMnemonic(KeyEvent.VK_C);
+        fileClose.setActionCommand("1");
+        fileClose.addActionListener(controller);
+
+        JMenuItem about = new JMenuItem("About");
+        about.setMnemonic(KeyEvent.VK_A);
+        about.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                AboutDialog ad = new AboutDialog();
+                ad.setVisible(true);
+            }
+        });
+        
+        JMenuItem pref = new JMenuItem("Preferences");
+        pref.setMnemonic(KeyEvent.VK_P);
+        pref.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                OptionDialog o = new OptionDialog();
+                o.setVisible(true);
+            }
+        });
+        
+        tools.add(pref);
+        
+        JMenuItem helpMe = new JMenuItem("Help Topics");
+        helpMe.setMnemonic(KeyEvent.VK_E);
+
+        file.add(fileClose);
+        help.add(about);
+        help.add(helpMe);
+
+        menubar.add(file);
+        menubar.add(tools);
+        menubar.add(help);
+
+        setJMenuBar(menubar);
+    }    
     public void update(Observable o, Object arg) {
-        textArea.append(model.getArea() + "\n");
-        textField.setText(model.getField());
+        
+        textField.setText("");
+        
+        locale = Locale.getDefault();
+        date = new Date();
+        String s = DateFormat.getTimeInstance(DateFormat.MEDIUM,
+                    locale).format(date);
+        
+        chatArea.append("[" + s + "]<Linguini>: " + model.getInput() + "\n");
+        chatArea.append("[" + s + "]<Gusteau>: " + model.getOutput() + "\n");
     }
 }
