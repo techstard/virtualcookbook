66e9eee0058d53474fac33c90523edcf216b2d32
anders <anders@lorentzen.se>
2008-04-17 09:05:51 +0000

    Här är en klass som kan koppla upp sig mot en databas. Än så länge funkar den bara för Anders, då han har databasen lokalt på sin bärbara dator. Tanken är att Tobbe lägger in den på sin server hemma hos sig senare så alla kan koppla upp sig mot den. /AL

---
+++ VirtualGusteau/test/DB_connect.java
@@ -0,0 +1,57 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * This class connects to the recipe database.
+ * @author Anders & Tobbe
+ * TIN171, 2008
+ */
+public class DB_connect {
+	
+	private String doWant;
+	private String doNotWant;
+	private String restrictions;
+	
+	public static void main(String args[]) {
+		Connection con = null;
+
+		try {
+			// Create the connection
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			// Notice the username and password, it's only configured for Anders... yet.
+			con = DriverManager.getConnection("jdbc:mysql://localhost/recipe_db",
+					"root", "vertrigo");
+			
+			// If this works, you are connected.
+			if(!con.isClosed())
+				System.out.println("Successfully connected to " +
+					"MySQL server using TCP/IP...");
+      
+			// Now for the real deal.
+			
+			// Create statement
+			Statement stmt = con.createStatement();
+		
+			// Enter your query.
+			ResultSet rset = stmt.executeQuery ("SELECT * "+
+														"FROM recipes ");
+			// Iterate through the result.
+			while (rset.next())
+				System.out.println ("arg2: " + rset.getString(2));
+
+		} catch(Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			System.err.println(e);
+		} finally {
+			try {
+				if(con != null)
+					con.close();
+			} catch(SQLException e) {
+				System.out.println(e);
+			}
+		}
+	}
+}
