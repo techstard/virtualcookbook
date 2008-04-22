5f7e86edb84bb116d10df775f0e9f863a7750e9d
Tobias.Alette <Tobias.Alette@0de4426a-6c4a-0410-a967-598ff313a736>
2008-04-22 14:15:20 +0000

    Ändrat  i DB__connect så nu går det att ansluta till databasen. Ni kanske måste importera ett library för mysql.

--- VirtualGusteau/src/virtualgusteau/DB_connect.java
+++ VirtualGusteau/src/virtualgusteau/DB_connect.java
@@ -1,3 +1,5 @@
+package virtualgusteau;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -20,38 +22,46 @@ public class DB_connect {
 	
 	private String warnings[];
 	private String specificDish;
-	
+	        
 	// This main method should be changed to a 
 	//  constructor class or something in the future.
-	public static void main(String args[]) {
+	public String connect() {
 		Connection con = null;
 
 		try {
 			// Create the connection
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			// Notice the username and password, it's only configured for Anders... yet.
-			con = DriverManager.getConnection("jdbc:mysql://localhost/recipe_db",
-					"root", "vertrigo");
-			
+			//con = DriverManager.getConnection("jdbc:mysql://localhost/recipe_db",
+			//		"root", "vertrigo");
+                        //Testing another MySQL server
+			con = DriverManager.getConnection("jdbc:mysql://193.11.241.134:3306/recipe_db",
+					"Gusteau", "gusteau");
+                        
 			// If this works, you are connected.
-			if(!con.isClosed())
-				System.out.println("Successfully connected to " +
-					"MySQL server using TCP/IP...");
+			if(!con.isClosed()) {
+				/*System.out.println("Successfully connected to " +
+					"MySQL server using TCP/IP...");*/
+                            return "connection succsessfull";
+                        } else {
+                            return "connection failed";
+                        }
       
 			// Now for the real deal.
 			
 			// Create statement
-			Statement stmt = con.createStatement();
+			/*Statement stmt = con.createStatement();
 		
 			// Enter your query.
 			ResultSet rset = stmt.executeQuery ("SELECT * FROM recipes ");
 			// Iterate through the result.
 			while (rset.next())
-				System.out.println ("arg2: " + rset.getString(2));
+				System.out.println ("arg2: " + rset.getString(1));*/
 
 		} catch(Exception e) {
 			System.err.println("Exception: " + e.getMessage());
 			System.err.println(e);
+                        return "Exception : " + e;
 		} finally {
 			try {
 				if(con != null)
