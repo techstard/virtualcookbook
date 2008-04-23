0e75ae767ae74539e5f69a4ed5dc6efab26ab44c
magnus.spanggard <magnus.spanggard@0de4426a-6c4a-0410-a967-598ff313a736>
2008-04-23 10:50:36 +0000

    I think that the knowledgebase is finished now. Atleast it contains what I can think of right now. //spanggar

--- VirtualGusteau/src/virtualgusteau/KnowledgeBase.java
+++ VirtualGusteau/src/virtualgusteau/KnowledgeBase.java
@@ -6,25 +6,26 @@
 package virtualgusteau;
 
 import java.util.LinkedList;
+import java.util.*;
+
 
 /**
  *
  * @author magnus
  */
 public class KnowledgeBase {
-    private LinkedList<Word> ingredients; //all ingredients mentioned to Gusteau
-    private LinkedList<Word> ingredients_nogo; //ingredients not wanted
-    private LinkedList<Word> defects; //defects such as vegetarian
-    private LinkedList<Word> portions; //how many people/portions
+    private LinkedList<Word> ingredientsWanted = new LinkedList<Word>(); //all ingredientsWanted mentioned to Gusteau
+    private LinkedList<Word> ingredientsNotWanted = new LinkedList<Word>(); //ingredientsWanted not wanted
+    private LinkedList<Word> defects = new LinkedList<Word>(); //defects such as vegetarian
+    private int nrOfPersons; //how many people
     
-
     /**
      * This function will try to add the ingredient to the kb and return a bool of its success.
      * @param ingredient is the ingredient which you want to add to the knowledge base.
      * @return true or false depending on if the ingredient was succesfully added to the kb or not.
      */
-    private boolean addIngredient(Word ingredient) {
-        if(ingredients.add(ingredient))
+    private boolean addIngredientWanted(Word ingredient) {
+        if(ingredientsWanted.add(ingredient))
             return true;
         else
             return false;
@@ -35,8 +36,32 @@ public class KnowledgeBase {
      * @param ingredient is the ingredient which you want to remove from the knowledge base.
      * @return true or false depending on if the ingredient was succesfullt removed from the kb or not.
      */
-    private boolean removeIngreident(Word ingredient) {
-        if(ingredients.remove(ingredient))
+    private boolean removeIngredidentWanted(Word ingredient) {
+        if(ingredientsWanted.remove(ingredient))
+            return true;
+        else
+            return false;
+    }
+    
+        /**
+     * This function will try to add the ingredient not wanted to the kb and return a bool of its success.
+     * @param ingredient is the ingredient not wanted which you want to add to the knowledge base.
+     * @return true or false depending on if the ingredient was succesfully added to the kb or not.
+     */
+    private boolean addIngredientNotWanted(Word ingredient) {
+        if(ingredientsNotWanted.add(ingredient))
+            return true;
+        else
+            return false;
+    }
+    
+    /**
+     * This function will try to remove the ingredient not wanted from the kb and return a bool of its success.
+     * @param ingredient is the ingredient not wanted which you want to remove from the knowledge base.
+     * @return true or false depending on if the ingredient was succesfullt removed from the kb or not.
+     */
+    private boolean removeIngreidentNotWanted(Word ingredient) {
+        if(ingredientsNotWanted.remove(ingredient))
             return true;
         else
             return false;
@@ -64,5 +89,64 @@ public class KnowledgeBase {
             return true;
         else
             return false;
+    }
+    
+    private boolean setNrOfPersons(int amount) {
+        int tmp = nrOfPersons;
+        if(nrOfPersons != amount)
+        {
+            nrOfPersons = amount;
+            if(tmp == nrOfPersons)
+                return false;
+            else
+                return true;
+        }
+        else
+            return true;
+    }
+    private int getNrOfPersons() {
+        return nrOfPersons;
+    }
+    
+    
+    public Iterator iWIterator() {
+                return new ingredientsWantedIterator();
+    }
+    public Iterator iNWIterator() {
+                return new ingredientsNotWantedIterator();
+    }
+    public Iterator dIterator() {
+                return new defectsIterator();
+    }
+    
+    private class ingredientsWantedIterator implements Iterator {
+        private int cnt = 0;
+        public boolean hasNext() {
+            return cnt < ingredientsWanted.size();
+        }
+        public Word next() {
+            return ingredientsWanted.get(cnt++);
+        }
+        public void remove(){}
+    }
+    private class ingredientsNotWantedIterator implements Iterator {
+        private int cnt = 0;
+        public boolean hasNext() {
+            return cnt < ingredientsNotWanted.size();
+        }
+        public Word next() {
+            return ingredientsNotWanted.get(cnt++);
+        }
+        public void remove(){}
+    }
+    private class defectsIterator implements Iterator {
+        private int cnt = 0;
+        public boolean hasNext() {
+            return cnt < defects.size();
+        }
+        public Word next() {
+            return defects.get(cnt++);
+        }
+        public void remove(){}
     }
 }
